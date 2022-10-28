bbf7f7553e2f56b25200430ca00ed7d59ffde366
Ivan Vikhrev <i.vikhrev@yadro.com>
2022-10-28 18:44:07 +0300

    add argument parsing and general tensor filling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+#include "args_handler.hpp"
 #include "logger.hpp"
 #include "statistics.hpp"
 
+#include <gflags/gflags.h>
 #include <torch/script.h>
 #include <torch/torch.h>
 
@@ -15,11 +17,53 @@
 using HighresClock = std::chrono::high_resolution_clock;
 using ns = std::chrono::nanoseconds;
 
-static inline uint64_t sec_to_ns(uint32_t duration) {
+namespace {
+constexpr char help_msg[] = "show the help message and exit";
+DEFINE_bool(h, false, help_msg);
+
+constexpr char model_msg[] = "path to an .onnx file with a trained model";
+DEFINE_string(m, "", model_msg);
+
+constexpr char input_msg[] =
+    "path to an input to process. The input must be an image and/or binaries, a folder of images and/or binaries.\n"
+    "                                                     Ex, \"input1:file1 input2:file2 input3:file3\" or just path "
+    "to the file or folder if model has one input";
+DEFINE_string(i, "", input_msg);
+
+constexpr char shape_msg[] = "shape for network input.\n"
+                             "                                                     Ex., "
+                             "\"input1[1,128],input2[1,128],input3[1,128]\" or just \"[1,3,224,224]\"";
+DEFINE_string(shape, "", shape_msg);
+
+constexpr char data_type_msg[] = "model input data type (options: f32, int32, int64).";
+DEFINE_string(dtype, "", data_type_msg);
+
+constexpr char time_msg[] = "time limit for inference in seconds";
+DEFINE_uint32(t, 0, time_msg);
+
+void parse(int argc, char *argv[]) {
+    gflags::ParseCommandLineFlags(&argc, &argv, false);
+    if (FLAGS_h || 1 == argc) {
+        std::cout << "onnxruntime_benchmark"
+                  << "\nOptions:"
+                  << "\n\t[-h]                                         " << help_msg
+                  << "\n\t[-help]                                      print help on all arguments"
+                  << "\n\t -m <MODEL FILE>                             " << model_msg
+                  << "\n\t[-i <INPUT>]                                 " << input_msg
+                  << "\n\t[-shape <[N,C,H,W]>]                         " << shape_msg
+                  << "\n\t[-dtype <dtype>]                             " << data_type_msg
+                  << "\n\t[-t <NUMBER>]                                " << time_msg;
+    }
+    if (FLAGS_m.empty()) {
+        throw std::invalid_argument{"-m <MODEL FILE> can't be empty"};
+    }
+}
+
+inline uint64_t sec_to_ns(uint32_t duration) {
     return duration * 1000000000LL;
 }
 
-static inline double ns_to_ms(std::chrono::nanoseconds duration) {
+inline double ns_to_ms(std::chrono::nanoseconds duration) {
     return static_cast<double>(duration.count()) * 0.000001;
 }
 
@@ -27,79 +71,105 @@ std::string format_double(const double number) {
     std::stringstream ss;
     ss << std::fixed << std::setprecision(2) << number;
     return ss.str();
-};
-
-template <class T>
-std::vector<T> read_data(const std::string &file_path, const std::vector<int> &shape) {
-    auto tensor_size = std::accumulate(shape.begin(), shape.end(), 1, std::multiplies<int>());
-    auto input_size = tensor_size * sizeof(T);
-    std::vector<T> tensor_data(tensor_size);
-    char *data = reinterpret_cast<char *>(tensor_data.data());
-    for (int b = 0; b < shape[0]; ++b) {
-        std::ifstream binary_file(file_path, std::ios_base::binary | std::ios_base::ate);
-        if (!binary_file) {
-            throw std::runtime_error("Can't open " + file_path);
-        }
-
-        auto file_size = static_cast<std::size_t>(binary_file.tellg());
-        if (file_size != input_size) {
-            throw std::invalid_argument("File " + file_path + " contains " + std::to_string(file_size) +
-                                        " bytes but the mdoel expects " + std::to_string(input_size));
-        }
-
-        binary_file.seekg(0, std::ios_base::beg);
-        if (!binary_file.good()) {
-            throw std::runtime_error("Can't read " + file_path);
-        }
-
-        binary_file.read(&data[b * input_size], input_size);
-    }
-
-    return tensor_data;
 }
 
-int main(int argc, const char *argv[]) {
+std::vector<std::vector<torch::jit::IValue>> get_input_tensors(
+    const std::vector<std::pair<std::string, std::vector<std::string>>> &input_files,
+    const std::map<std::string, std::vector<int>> &shapes,
+    const std::map<std::string, torch::Dtype> &dtypes) {
+    int inputs_num = input_files[0].second.size();
+    std::vector<std::vector<torch::jit::IValue>> inputs(inputs_num);
+    for (int i = 0; i < inputs_num; ++i) {
+        logger::info << "Input config " << i << logger::endl;
+        for (const auto &[input_name, files_list] : input_files) {
+            logger::info << "\t" << input_name << logger::endl;
+            logger::info << "\t\t" << files_list[i] <<  logger::endl;
+            auto size =
+                std::accumulate(shapes.at(input_name).begin(), shapes.at(input_name).end(), 1, std::multiplies<int>());
+            auto tensor = torch::from_file(files_list[i],
+                                           false,
+                                           size,
+                                           torch::TensorOptions().dtype(dtypes.at(input_name)).device(torch::kCPU));
+            inputs[i].push_back(tensor.view({1, 128}));
+        }
+    }
+    return inputs;
+}
+} // namespace
+
+// template <class T>
+// std::vector<T> read_data(const std::string &file_path, const std::vector<int> &shape) {
+//     auto tensor_size = std::accumulate(shape.begin(), shape.end(), 1, std::multiplies<int>());
+//     auto input_size = tensor_size * sizeof(T);
+//     std::vector<T> tensor_data(tensor_size);
+//     char *data = reinterpret_cast<char *>(tensor_data.data());
+//     for (int b = 0; b < shape[0]; ++b) {
+//         std::ifstream binary_file(file_path, std::ios_base::binary | std::ios_base::ate);
+//         if (!binary_file) {
+//             throw std::runtime_error("Can't open " + file_path);
+//         }
+
+//         auto file_size = static_cast<std::size_t>(binary_file.tellg());
+//         if (file_size != input_size) {
+//             throw std::invalid_argument("File " + file_path + " contains " + std::to_string(file_size) +
+//                                         " bytes but the mdoel expects " + std::to_string(input_size));
+//         }
+
+//         binary_file.seekg(0, std::ios_base::beg);
+//         if (!binary_file.good()) {
+//             throw std::runtime_error("Can't read " + file_path);
+//         }
+
+//         binary_file.read(&data[b * input_size], input_size);
+//     }
+
+//     return tensor_data;
+// }
+
+int main(int argc, char *argv[]) {
     try {
+        logger::info << "Parsing input arguments" << logger::endl;
+        parse(argc, argv);
+        std::map<std::string, std::vector<int>> shapes;
+        for (const auto &[input_name, shape] : args::parse_parameter_string(FLAGS_shape)) {
+            shapes.emplace(input_name, args::string_to_vec<int>(shape, ','));
+        }
+
+        std::map<std::string, torch::Dtype> dtypes;
+        for (const auto &[input_name, type] : args::parse_parameter_string(FLAGS_dtype)) {
+            if (type == "fp32") {
+                dtypes.emplace(input_name, torch::kFloat32);
+            }
+            else if (type == "int32") {
+                dtypes.emplace(input_name, torch::kInt32);
+            }
+            else if (type == "int64") {
+                dtypes.emplace(input_name, torch::kInt64);
+            }
+        }
+
+        logger::info << "Reading input files" << logger::endl;
+        auto input_files = args::parse_input_files_arguments(gflags::GetArgvs());
+
         logger::info << "PyTorch version: " << TORCH_VERSION_MAJOR << "." << TORCH_VERSION_MINOR << "."
                      << TORCH_VERSION_PATCH << logger::endl;
-        logger::info << "Loading model " << argv[1] << logger::endl;
+        logger::info << "Loading model " << FLAGS_m << logger::endl;
+
         torch::jit::script::Module module;
-        module = torch::jit::load(argv[1]);
+        module = torch::jit::load(FLAGS_m);
         module.eval();
 
-        logger::info << "Set no grad guard" << logger::endl;
+        logger::info << "Disabling the gradient computation" << logger::endl;
         torch::NoGradGuard no_grad;
 
         logger::info << "Prepare input tensors" << logger::endl;
-        std::vector<std::vector<torch::jit::IValue>> inputs;
-        std::vector<std::vector<int64_t>> data_buffer;
-        {
-            auto input_ids = read_data<int64_t>("/home/ivikhrev/dev/data/bert_base_ner/input_ids_1_128.bin", {1, 128});
-            auto attention_mask =
-                read_data<int64_t>("/home/ivikhrev/dev/data/bert_base_ner/attention_mask_1_128.bin", {1, 128});
-            auto token_type_ids =
-                read_data<int64_t>("/home/ivikhrev/dev/data/bert_base_ner/token_type_ids_1_128.bin", {1, 128});
+        auto inputs = get_input_tensors(input_files, shapes, dtypes);
 
-            // auto input_ids_t = torch::from_blob(input_ids.data(),
-            //                                     {1, 128},
-            //                                     torch::TensorOptions().dtype(torch::kInt64).device(torch::kCPU));
-            auto input_ids_t = torch::from_file("/home/ivikhrev/dev/data/bert_base_ner/input_ids_1_128.bin",
-                                                false,
-                                                128,
-                                                torch::TensorOptions().dtype(torch::kInt64).device(torch::kCPU));
-            auto attention_mask_t = torch::from_blob(attention_mask.data(),
-                                                     {1, 128},
-                                                     torch::TensorOptions().dtype(torch::kInt64).device(torch::kCPU));
-            auto token_type_ids_t = torch::from_blob(token_type_ids.data(),
-                                                     {1, 128},
-                                                     torch::TensorOptions().dtype(torch::kInt64).device(torch::kCPU));
-            data_buffer.push_back(std::move(attention_mask));
-            data_buffer.push_back(std::move(token_type_ids));
-            inputs.push_back({input_ids_t.view({1,128}), attention_mask_t, token_type_ids_t});
-        }
-        std::vector<double> latencies;
         // set time limit
-        uint32_t time_limit_sec = 5;
+        uint32_t time_limit_sec = FLAGS_t;
+        if (time_limit_sec == 0) {
+            time_limit_sec = 60;
+        }
         uint64_t time_limit_ns = sec_to_ns(time_limit_sec);
 
         logger::info << "Measuring model performance" << logger::endl;
@@ -109,6 +179,7 @@ int main(int argc, const char *argv[]) {
         auto first_inference_time = ns_to_ms(HighresClock::now() - infer_start_time);
         logger::info << "Warming up inference took " << format_double(first_inference_time) << " ms" << logger::endl;
 
+        std::vector<double> latencies;
         int64_t iteration = 0;
         auto start_time = HighresClock::now();
         auto uptime = std::chrono::duration_cast<ns>(HighresClock::now() - start_time).count();
@@ -119,8 +190,10 @@ int main(int argc, const char *argv[]) {
             uptime = std::chrono::duration_cast<ns>(HighresClock::now() - start_time).count();
             ++iteration;
         }
-        auto output = module.forward(inputs[0]).toTuple()->elements()[0].toTensor();
-        logger::info << output << logger::endl;
+
+        // check outputs
+        // auto output = module.forward(inputs[0]).toTuple()->elements()[0].toTensor();
+        // logger::info << output << logger::endl;
         Metrics metrics(latencies, 1);
 
         // Performance metrics report
